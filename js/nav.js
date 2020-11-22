c3dae215a3c08c7048c42af0f1fc75f500345d94
AbhijitNambiar <abhijitnambiar26@gmail.com>
2020-11-22 12:41:40 +0530

    made changes in index.html

--- js/nav.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 10) {
-        $(".navbar").addClass("nav-scroll");
-      } else {
-        $(".navbar").removeClass("nav-scroll");
-      }
-});
